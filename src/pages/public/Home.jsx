9dd18e6e1798336b7c42c7e8c2386e6d104c7f24
GUILLEN Jean-Luc (EXT CGI) <PJGN05751@commun.ad.sncf.fr>
2026-07-06 16:40:17 +0200

    formulaire pour lien et chanteur

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>🎼 Il 2tait une voix dans l'est</h1>
+      <h1>🎼 Il était une voix dans l'est</h1>
       <p>Accès via lien personnel uniquement</p>
     </div>
   );
